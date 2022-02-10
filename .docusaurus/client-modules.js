a8d828d2402ab0997e755dc5ff64e1bbf43612ec
Cong Hai <hai@appfast.io>
2022-02-10 17:15:15 +0700

    update

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Volumes/Data/Appfast/appfast-docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Volumes/Data/Appfast/appfast-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Volumes/Data/Appfast/appfast-docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Volumes/Data/Appfast/appfast-docs/src/css/custom.css'),
-];
