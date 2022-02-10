f34a3dd81785bd5af8260d5c6e6261cd00bf05ca
ThienTrinh97 <thien.tt@appfast.io>
2022-02-10 17:41:06 +0700

    commit

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/thientrinhtrong/Documents/GitHub/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/thientrinhtrong/Documents/GitHub/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/thientrinhtrong/Documents/GitHub/docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/thientrinhtrong/Documents/GitHub/docs/src/css/custom.css"),
-];
